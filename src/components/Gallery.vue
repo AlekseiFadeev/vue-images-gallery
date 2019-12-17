3e182dbcae7ba616b0d530921e79f5f5f6fe80c8
Aleksei Fadeev <33324706+AlekseiFadeev@users.noreply.github.com>
2019-12-17 03:14:48 +0300

    master

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="gallery">
-        Thats was gallery.
+        <div v-for="image in images" :key="image.id" class="card">
+          <div class="image" :style="{ backgroundImage: 'url(' + image.urls.small + ')' }"></div>
+          <div class="author">{{ image.user.first_name }}</div>
+        </div>
     </div>
 </template>
 
@@ -12,7 +15,11 @@ const key = "9404ae5ed465bacd0a7946af14a336d213fc0211605817fce330ba83886ce9a4"
 
 export default {
   name: 'Gallery',
-  images: [],
+  data() {
+    return {
+      images: []
+    }
+  },
   mounted() {
     axios
       .get('/photos/', {
@@ -31,3 +38,30 @@ export default {
 }
 </script>
 
+<style scoped>
+  .gallery {
+    width: 1400px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+
+  .card {
+    margin: 20px;
+    width: 300px;
+    height: 300px;
+  }
+
+  .image {
+    width: 100%;
+    height: 100%;
+  }
+
+  .author{
+    margin-top: 3px;
+    text-align: left;
+    font-size: 11px;
+    color: gray;
+  }
+</style>
